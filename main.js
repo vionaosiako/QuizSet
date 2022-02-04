49c8520586fc745edb93f97d3210c8b96c11a6a7
vionaosiako <viona.osiako@student.morigaschool.com>
2022-02-04 23:38:12 +0300

    THos commit will correct the error on the name of varriables in the js file

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 function check(){
     var answer =0;
     var que1 = document.quiz.question1.value;
-    var que2 = document.quiz.question1.value;
-    var que3 = document.quiz.question1.value;
-    var que4 = document.quiz.question1.value;
-    var que5 = document.quiz.question1.value;
-    var que6 = document.quiz.question1.value;
-    var que7 = document.quiz.question1.value;
-    var que8 = document.quiz.question1.value;
-    var que9 = document.quiz.question1.value;
-    var que10 = document.quiz.question1.value;
+    var que2 = document.quiz.question2.value;
+    var que3 = document.quiz.question3.value;
+    var que4 = document.quiz.question4.value;
+    var que5 = document.quiz.question5.value;
+    var que6 = document.quiz.question6.value;
+    var que7 = document.quiz.question7.value;
+    var que8 = document.quiz.question8.value;
+    var que9 = document.quiz.question9.value;
+    var que10 = document.quiz.question10.value;
 
-    
+    if (que1 == "javascript")
+    {answer++}
+    document(answer);
 }
