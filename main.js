4ac8d1e3eef2573a69f812b2492fcb3b1b976a93
vionaosiako <viona.osiako@student.morigaschool.com>
2022-02-04 23:30:03 +0300

    This commit will add check function in the js file

---
+++ main.js
@@ -0,0 +1,15 @@
+function check(){
+    var answer =0;
+    var que1 = document.quiz.question1.value;
+    var que2 = document.quiz.question1.value;
+    var que3 = document.quiz.question1.value;
+    var que4 = document.quiz.question1.value;
+    var que5 = document.quiz.question1.value;
+    var que6 = document.quiz.question1.value;
+    var que7 = document.quiz.question1.value;
+    var que8 = document.quiz.question1.value;
+    var que9 = document.quiz.question1.value;
+    var que10 = document.quiz.question1.value;
+
+    
+}
